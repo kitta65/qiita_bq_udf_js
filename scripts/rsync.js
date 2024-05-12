3f280a8a259a7becdfb4d3b81301688fc8b60c4d
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-05-12 12:46:52 +0900

    fix

--- scripts/rsync.js
+++ scripts/rsync.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env bun
 import {$} from "bun"
-import * as path from "path"
+import * as path from "node:path"
 
 const root = path.resolve(__dirname, "../")
 process.chdir(root)
 
 const bucket = Bun.env.BUCKET_NAME
-console.log(`uploading to ${bucket}...`)
-await $`gsutil cp out/* gs://${bucket}`
+await $`gsutil rsync out gs://${bucket}`
